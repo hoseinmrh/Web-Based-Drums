c005ae61e03dbbfd24fe93a1f9ca60c706492581
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2022-08-21 18:11:24 +0430

    First init

---
+++ index.js
@@ -0,0 +1,41 @@
+
+for(var i = 0; i < document.querySelectorAll(".drum").length ; i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+        break;
+      case "a":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+        break;
+      case "s":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+        break;
+      case "d":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+        break;
+      case "j":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+        break;
+      case "k":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+        break;
+      case "l":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+        break;
+    default:
+
+    }
+    // var audio = new Audio("sounds/crash.mp3");
+    // audio.play();
+  });
+}
